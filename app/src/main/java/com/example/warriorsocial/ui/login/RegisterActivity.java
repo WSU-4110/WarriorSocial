133f78742c70f2d7e874c4801c3729b2563dc924
Chloe Matthews <gk6153@wayne.edu>
2021-02-05 22:55:24 -0500

    Initial commit

---
+++ app/src/main/java/com/example/warriorsocial/ui/login/RegisterActivity.java
@@ -0,0 +1,16 @@
+package com.example.warriorsocial.ui.login;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.warriorsocial.R;
+
+public class RegisterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+    }
+}
