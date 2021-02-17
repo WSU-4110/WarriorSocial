4e84f3cf6269f1871885ee852e70b3f09c901e0b
Chloe Matthews <gk6153@wayne.edu>
2021-02-16 23:42:45 -0500

    Linked Calendar date clicks to generating a list of activities in Recycler View on Home Fragment. Updated look of Login UI. Changed icons/fragments on Home Fragment (Added Discover and Organizations)

--- app/src/main/java/com/example/warriorsocial/ui/discover/DiscoverViewModel.java
+++ app/src/main/java/com/example/warriorsocial/ui/discover/DiscoverViewModel.java
@@ -1,16 +1,16 @@
-package com.example.warriorsocial.ui.dashboard;
+package com.example.warriorsocial.ui.discover;
 
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
-public class DashboardViewModel extends ViewModel {
+public class DiscoverViewModel extends ViewModel {
 
     private MutableLiveData<String> mText;
 
-    public DashboardViewModel() {
+    public DiscoverViewModel() {
         mText = new MutableLiveData<>();
-        mText.setValue("This is dashboard fragment");
+        mText.setValue("This is the discover fragment");
     }
 
     public LiveData<String> getText() {
