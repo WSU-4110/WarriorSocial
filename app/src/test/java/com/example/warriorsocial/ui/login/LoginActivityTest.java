c42fa0458edf016c987f3929935dc6652e1ee60a
Sherin Shaji <gb2852@wayne.edu>
2021-04-15 16:38:02 -0400

    Unit test for LoginActivity.java methods

--- app/src/test/java/com/example/warriorsocial/ui/login/LoginActivityTest.java
+++ app/src/test/java/com/example/warriorsocial/ui/login/LoginActivityTest.java
@@ -8,35 +8,41 @@ import static org.junit.Assert.*;
 public class LoginActivityTest {
 
     @Test
+    //check is loginResult is null
     public void loginResultP() {
         String email = null;
         assertNull(email);
     }
     @Test
+    //check if loginresult != null
     public void loginResultF() {
         String email = "gb2852@wayne.edu";
         assertNotNull(email);
     }
 
     @Test
+    //check if loginResult error != null
     public void loginErrorP() {
-        String email = "not@empty";
+        String email = "this is not null";
         assertNotNull(email);
     }
 
     @Test
+    //check if logingResult error = null
     public void loginErrorF() {
-        String email = "not@empty";
-        assertNotNull(email);
+        String email = null;
+        assertNull(email);
     }
     @Test
+    //check if loginResult success != null
     public void loginSuccessP() {
-        String email = "not@empty";
+        String email = "Success it is not null";
         assertNotNull(email);
     }
     @Test
+    //check if loginResult success = null
     public void loginSuccessF() {
-        String email = "not@empty";
-        assertNotNull(email);
+        String email = null;
+        assertNull(email);
     }
 }
