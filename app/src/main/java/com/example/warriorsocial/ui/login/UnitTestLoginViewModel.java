5653ce1ebc3a2eb4c566c2f6e023519ae48abb96
Rahul Sheth <gj6548@wayne.edu>
2021-04-15 23:45:55 -0400

    Unit Test for LoginViewModel

---
+++ app/src/main/java/com/example/warriorsocial/ui/login/UnitTestLoginViewModel.java
@@ -0,0 +1,66 @@
+package com.example.warriorsocial.ui.login;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class UnitTestLoginViewModel {
+
+
+    @BeforeClass
+    public static void setUpBeforeClass() throws Exception {
+    }
+
+    @AfterClass
+    public static void tearDownAfterClass() throws Exception {
+    }
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+
+    @SuppressWarnings("deprecation")
+    @Test
+    public void testLoginViewModel() {
+        Object[] loginRepository = null;
+        assertEquals(loginRepository, loginRepository);
+    }
+
+    @Test
+    public void testGetLoginFormState() {
+        boolean loginFormState = false;
+        assertFalse(loginFormState);
+    }
+
+    @Test
+    public void testGetLoginResult() {
+        boolean loginResult = false;
+        assertFalse(loginResult);
+    }
+
+    @SuppressWarnings("deprecation")
+    @Test
+    public void testLogin() {
+        Object[] username;
+        Object[] loginUser = null;
+        username = loginUser;
+        assertEquals(username, loginUser);
+    }
+
+    @Test
+    public void testLoginDataChanged() {
+        boolean username = false;
+        boolean password = false;
+        assertFalse(username);
+        assertFalse(password);
+    }
+
+}
